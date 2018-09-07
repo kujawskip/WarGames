159d07d5b98d1ac906a909aa4219fcbf9294eb0b
kujawskip <patryk.kujawski93@gmail.com>
2018-09-07 19:37:20 +0200

    Added placement

--- placementtab.h
+++ placementtab.h
@@ -1,11 +1,12 @@
 #ifndef PLACEMENTTAB_H
 #define PLACEMENTTAB_H
 #include <QtWidgets>
+#include "placement.h"
 class PlacementTab : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit PlacementTab(QWidget *parent = 0);
+    explicit PlacementTab(QWidget *parent, Placement& placement);
 };
 #endif // PLACEMENTTAB_H
