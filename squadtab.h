0f7222a6346789b1e5ee9d1aaa69dcb96fbc3ffc
kujawskip <patryk.kujawski93@gmail.com>
2018-09-07 19:38:57 +0200

    Added army

--- squadtab.h
+++ squadtab.h
@@ -1,11 +1,12 @@
 #ifndef MAPTAB_H
 #define MAPTAB_H
 #include <QtWidgets>
+#include "army.h"
 class SquadTab : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit SquadTab(QWidget *parent = 0);
+    explicit SquadTab(QWidget *parent, Army& redArmy,Army& blueArmy);
 };
 #endif // MAPTAB_H
